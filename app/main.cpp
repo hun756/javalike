6fdf3ea5847bd3b861397c3a00fefbb11515bb54
Mehmet Şükrü Ekemen <56188619+hun756@users.noreply.github.com>
2022-02-15 19:09:34 +0300

    Added Samples to new executable.

--- app/main.cpp
+++ app/main.cpp
@@ -1,29 +1,47 @@
 #include <chrono>
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <thread>
 #include <vector>
 
+
 #include <Math.hpp>
 #include <helpers/JavalikeType.hpp>
 
-namespace App
+namespace
 {
-	_interface Deneme{};
-	static void Main(std::vector<std::string> args)
+	void mathRand()
 	{
-		(void)fprintf(stdout, "%s", "Welcome to JavaLike Project");
-
 		std::thread th([]() {
 			while(true)
 			{
-				(void)fprintf(stdout, "%lf\n", Javalike::Lang::Math::random());
+				auto begin = std::chrono::high_resolution_clock::now();
+				auto val = (float)rand() / (float)(RAND_MAX);
+				auto end = std::chrono::high_resolution_clock::now();
+
+				std::cout
+					<< "Time Elapsed : "
+					<< std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count()
+					<< std::endl;
+
+				(void)fprintf(stdout, "%lf\n", val);
+
 				std::this_thread::sleep_for(std::chrono::milliseconds(500));
 			}
 		});
 
 		th.join();
+	}
+} // namespace
+
+namespace App
+{
+	interface Deneme { };
+	static void Main(std::vector<std::string> args)
+	{
+		(void)fprintf(stdout, "%s", "Welcome to JavaLike Project");
 	}
 } // namespace App
 
